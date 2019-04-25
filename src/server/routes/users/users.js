84464573609e8d26ea1866c417a52626573fcf6d
Matthew Crocker <matthewcroc@gmail.com>
2019-04-25 15:39:57 -0500

    Deploy to heroku

--- src/server/routes/users/users.js
+++ src/server/routes/users/users.js
@@ -2,10 +2,6 @@ const Router = require('express-promise-router');
 
 const router = new Router();
 
-router.get('/', (req, res) => {
-  res.send('User login here?');
-});
-
 router.get('/getUsername', async (req, res) => {
   res.send({ username: 'User' });
 });
